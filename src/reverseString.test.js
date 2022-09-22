619622895e71f88b28b6635807881600f8192b3e
Daniel Gallardo <dgallardo.c@outlook.com>
2022-09-22 00:40:32 -0500

    Add tests

---
+++ src/reverseString.test.js
@@ -0,0 +1,26 @@
+import reverseString from "./reverseString";
+
+test('empty call', () =>
+  expect(reverseString()).toBe("")
+)
+
+test('output is string', () => {
+  expect(typeof capitalize(9)).toBe("string");
+})
+
+test('numeric output', () =>
+  expect(reverseString(90)).toBe("09")
+)
+
+
+test('"" to "" ', () =>
+  expect(reverseString("")).toBe("")
+)
+
+test('" " to " " ', () =>
+  expect(reverseString(" ")).toBe(" ")
+)
+
+test('"reverse" to "esrever" ', () =>
+  expect(reverseString("reverse")).toBe("")
+)
