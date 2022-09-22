e075b650d3230b423d56caa6db150f1e1c3acc55
Daniel Gallardo <dgallardo.c@outlook.com>
2022-09-22 00:42:40 -0500

    Change name of variable and type to constant

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,9 +1,9 @@
 function capitalize(string = "") {
-  let array = string.toString().split("").reduce((str, v, i) => {
+  const newString = string.toString().split("").reduce((str, v, i) => {
     if (i == 0) return str + v.toUpperCase();
     else return str + v.toLowerCase();
   }, "");
-  return array;
+  return newString;
 }
 
 export default capitalize;
