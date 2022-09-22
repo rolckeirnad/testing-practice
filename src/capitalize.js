1ff0c0bcca80fca9e3f358fadceb79be0903f502
Daniel Gallardo <dgallardo.c@outlook.com>
2022-09-22 00:23:17 -0500

    Add capitalize functionality

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,5 +1,9 @@
-function capitalize(string) {
-  return string;
+function capitalize(string = "") {
+  let array = string.toString().split("").reduce((str, v, i) => {
+    if (i == 0) return str + v.toUpperCase();
+    else return str + v.toLowerCase();
+  }, "");
+  return array;
 }
 
 export default capitalize;
