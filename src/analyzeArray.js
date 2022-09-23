4b8c4f58104abdbc95149835a07dc77bf5f4f27d
Daniel Gallardo <dgallardo.c@outlook.com>
2022-09-23 15:18:45 -0500

    Add functionality

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,5 +1,10 @@
-function analyzeArray(arr) {
-  const obj = {};
+function analyzeArray(arr = []) {
+  const obj = arr.reduce((obj, curr) => {
+    obj.average = obj.average + (curr / obj.length);
+    if (curr > obj.max) obj.max = curr;
+    if (curr < obj.min) obj.min = curr;
+    return obj;
+  }, { average: 0, min: arr[0] || 0, max: arr[0] || 0, length: arr.length });
   return obj;
 }
 
