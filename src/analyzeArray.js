395b1ea29e411c0c03db3c567a30b7f1073a1833
Daniel Gallardo <dgallardo.c@outlook.com>
2022-09-23 14:38:00 -0500

    Add tests for analyzeArray

---
+++ src/analyzeArray.js
@@ -0,0 +1,6 @@
+function analyzeArray(arr) {
+  const obj = {};
+  return obj;
+}
+
+export default analyzeArray;
---
+++ src/analyzeArray.test.js
@@ -0,0 +1,23 @@
+import analyzeArray from "./analyzeArray";
+
+test('Function returns an object', () =>
+  expect(typeof analyzeArray()).toBe('object')
+)
+
+test('Contains the following properties: average, min, max and length', () =>
+  expect(analyzeArray()).toMatchObject({
+    average: expect.any(Number),
+    min: expect.any(Number),
+    max: expect.any(Number),
+    length: expect.any(Number),
+  })
+)
+
+test('[1,8,3,4,2,6]', () =>
+  expect(analyzeArray()).toMatchObject({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  })
+)
