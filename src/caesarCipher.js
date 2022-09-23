e1fcec7cfeaf7b85b716296327ae8786dadb9661
Daniel Gallardo <dgallardo.c@outlook.com>
2022-09-23 12:33:40 -0500

    Update function to receive a shift value

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,9 +1,10 @@
-function caesarCipher(string = "") {
+function caesarCipher(string = "", k = 1) {
+  const shift = k % 26;
   const arr = string.split("");
   const result = arr.reduce((str, char) => {
     if (char.toLowerCase() === char.toUpperCase()) return str + char; // Return punctuation character
     const base = char.charCodeAt() < 97 ? 65 : 97; // Check if is lowercase or uppercase
-    let offset = base + (((char.charCodeAt() - base) + 1) % 26); //Calculate offset
+    let offset = base + (((char.charCodeAt() - base) + shift) % 26); // Calculate offset
     return str + String.fromCharCode(offset);
   }, "");
   return result;
